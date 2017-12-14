0446fecc479617bea5f96f7e535a8cfff345415c
sebasarango1180 <sebas.arango@experimentality.co>
2017-12-14 16:17:17 -0500

    Cropper adapted to IP cameras.

--- cropper.py
+++ cropper.py
@@ -4,48 +4,56 @@ import os
 
 cascade_path = "/home/experimentality/openCV/opencv/data/haarcascades/"
 cascade = "haarcascade_frontalface_alt.xml"
-cropping_path = "/home/experimentality/Documents/Inteligencia/final_inteligencia/Cropped/"
-original = "/home/experimentality/Documents/Inteligencia/final_inteligencia/Original/"
-validator = "/home/experimentality/Documents/Inteligencia/final_inteligencia/Validation/"
+cropping_path = "/home/experimentality/Documents/Degree work/Software/employee_classifier/Cropped/"
+original = "/home/experimentality/Documents/Degree work/Software/employee_classifier/Original/"
+validator = "/home/experimentality/Documents/Degree work/Software/employee_classifier/Validation/"
 
 face = cv2.CascadeClassifier(cascade_path + cascade)
+
+cam_1 = cv2.VideoCapture("rtsp://admin:admin@172.16.1.252:554")
 
 settings = {
     'scaleFactor': 1.1,
     'minNeighbors': 3,
     # 'flags': cv2.cv.CV_HAAR_FIND_BIGGEST_OBJECT | cv2.cv.CV.HAAR_DO_ROUGH_SEARCH  # OpenCV 2
     'flags': cv2.CASCADE_FIND_BIGGEST_OBJECT | cv2.CASCADE_DO_ROUGH_SEARCH,  # OpenCV 3
-    'minSize': (40, 40)
+    'minSize': (30, 30)
 }
 
 
-def crop_face(face, box, n, label):
+def crop_face(face, box, n):
     # Box = [x y w h]
     cropped = face[box[1]:box[1] + box[3], box[0]:box[0] + box[2]]  # First Y coords, then X coords.
     cropped = cv2.cvtColor(cropped,cv2.COLOR_BGR2HSV) #Convierto a HSV
     cropped = cv2.resize(cropped, (120, 120))
 
-    crop_name = cropping_path + label + '.' + datetime.now().isoformat() + "_face_" + str(n) + ".png"
+    crop_name = cropping_path + '.' + datetime.now().isoformat() + "_face_" + str(n) + ".png"
     cv2.imwrite(crop_name, cropped)
 
 
 ###########################################################
 
-for imagePath in os.listdir(original): # Take original pics, detect faces and save croppings.
-    # load the image and extract the class label (assuming that our
-    # path as the format: /path/to/dataset/{class}.{image_num}.jpg
-    image = cv2.imread(original + imagePath)
-    label = imagePath.split(os.path.sep)[-1].split(".")[0]
+while True:
+
+    ret, image = cam_1.read()
 
     detected = face.detectMultiScale(image, **settings)  # Returns list of rectangles in the image.
-
     print(detected)
+
     if len(detected):
         n = 1
         for faces in detected:
-            crop_face(image, faces, n, label)
+            crop_face(image, faces, n)
 
             n += 1
 
+
+    cv2.imshow('Cropper', image)
+
+    if cv2.waitKey(5) != -1:
+        break
+
     else:
-        print('No faces found')
+        print "No faces found"
+
+cv2.destroyWindow("Cropper")
